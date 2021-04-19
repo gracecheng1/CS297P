156a356582811f5e880ada58c904a19df7e0f3b8
Shiheng-Wang <shihengwang@umail.ucsb.edu>
2021-04-19 16:52:04 -0700

    Project Created

---
+++ Focus/Focus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Focus
+//
+//  Created by Shiheng Wang on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Focus/Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Shiheng Wang on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
