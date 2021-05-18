4571764f4bdd0acfbb667c07772807170c45fd04
Shiheng-Wang <shihengwang@umail.ucsb.edu>
2021-05-18 16:38:13 -0700

    Update History

--- Focus/Focus/History.swift
+++ Focus/Focus/History.swift
@@ -8,23 +8,43 @@
 import SwiftUI
 
 struct History: View {
+    @State private var date = Date()
+    @State private var focusTime = "No study at this date!"
+    
     var body: some View {
-        HStack{
+        VStack {
+            Text("History")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width, height: 100, alignment: .center)
+                .background(LinearGradient(gradient: Gradient(colors: [Color(0xFD716A), Color(0xFED363)]), startPoint: .bottomLeading, endPoint: .topTrailing))
             Spacer()
-            NavigationLink(destination: CountDown().navigationBarHidden(true)) {
-                EmptyTab(current:"Study")
+            DatePicker("Start Date",
+                       selection: $date,
+                       displayedComponents: [.date])
+                .datePickerStyle(GraphicalDatePickerStyle())
+            Spacer()
+            Text(focusTime)
+            Spacer()
+            HStack{
+                Spacer()
+                NavigationLink(destination: CountDown().navigationBarHidden(true)) {
+                    EmptyTab(current:"Study")
+                }
+                Spacer()
+                NavigationLink(destination: MyZoo().navigationBarHidden(true)) {
+                    EmptyTab(current:"MyZoo")
+                }
+                Spacer()
+                NavigationLink(destination: Rank().navigationBarHidden(true)) {
+                EmptyTab(current:"Rank")
+                }
+                Spacer()
+                CurrentTab(current:"History")
             }
-            Spacer()
-            NavigationLink(destination: MyZoo().navigationBarHidden(true)) {
-                EmptyTab(current:"MyZoo")
-            }
-            Spacer()
-            NavigationLink(destination: Rank().navigationBarHidden(true)) {
-            EmptyTab(current:"Rank")
-            }
-            Spacer()
-            CurrentTab(current:"History")
         }
+        .padding(.top, -15)
     }
 }
 
